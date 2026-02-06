9a0ecc33e21cde99031b31c4636ce6289f2256fd
Nah Wu <nah.wu@stengg.com>
2026-02-06 11:38:08 +0800

    Upgraded to nextjs 16

--- nextjs_server/next.config.js
+++ nextjs_server/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // ESLint is now configured via .eslintrc or package.json scripts
 }
 
 module.exports = nextConfig
